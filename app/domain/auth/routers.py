d1cf32663711ebb81aec1c8f77c914e0129413e3
Lim Jihwan <ghwn5936@gmail.com>
2022-02-06 15:16:01 +0900

    Change status code

--- app/domain/auth/routers.py
+++ app/domain/auth/routers.py
@@ -15,8 +15,8 @@ async def create_token(form: schemas.LoginForm, db: Session = Depends(get_db)):
     """토큰을 발급합니다."""
     user = get_user_by_username(db, form.username)
     if not user:
-        raise HTTPException(status_code=404, detail=f"The user '{form.username}' does not exist.")
+        raise HTTPException(status_code=400, detail=f"The user '{form.username}' does not exist.")
     if not verify_password(form.password, user.hashed_password):
-        raise HTTPException(status_code=401, detail="Password does not match.")
+        raise HTTPException(status_code=400, detail="Password does not match.")
     access_token = create_access_token({"username": user.username})
     return {"token_type": "Bearer", "access_token": access_token}
