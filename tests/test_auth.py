98d5ce049fcbf597a5a28079f1c25932c6a63ed7
Lim Jihwan <ghwn5936@gmail.com>
2022-02-18 23:10:01 +0900

    Comment all tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,13 +1,13 @@
-def test_login(guest_client, user):
-    response = guest_client.post(
-        url="/auth/token",
-        json={
-            "username": user.username,
-            "password": "password",
-        },
-    )
-    assert response.status_code == 200
-
-    response_json = response.json()
-    assert response_json["access_token"]
-    assert response_json["token_type"] == "Bearer"
+# def test_login(guest_client, user):
+#     response = guest_client.post(
+#         url="/auth/token",
+#         json={
+#             "username": user.username,
+#             "password": "password",
+#         },
+#     )
+#     assert response.status_code == 200
+#
+#     response_json = response.json()
+#     assert response_json["access_token"]
+#     assert response_json["token_type"] == "Bearer"
