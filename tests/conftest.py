c72aba62828c72ce0d32835982f76b7d41341de8
Lim Jihwan <ghwn5936@gmail.com>
2022-02-19 01:37:21 +0900

    Rewrite tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,120 +1,81 @@
-# import pytest
-# from fastapi.testclient import TestClient
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.sql.expression import text
-#
-# from app.configs import ROOT_DIR
-# from app.database import Base
-# from app.dependencies import get_db
-# from app.domain.users.crud import create_user
-# from app.domain.users.schemas import UserCreate
-# from app.main import create_app
-# from app.security import create_access_token
-#
-# DATABASE_URL = f"sqlite:///{ROOT_DIR / 'test.db'}"
-#
-# engine = create_engine(DATABASE_URL, echo=True, connect_args={"check_same_thread": False})
-# TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-#
-# Base.metadata.create_all(bind=engine)
-#
-#
-# @pytest.fixture(scope="function", autouse=True)
-# def run_around_tests():
-#     # Setup code
-#     with engine.connect() as conn:
-#         conn.execute(text("delete from users"))
-#     yield
-#     # Teardown code
-#
-#
-# @pytest.fixture(scope="session")
-# def app():
-#     def override_get_db():
-#         db = TestingSessionLocal()
-#         try:
-#             yield db
-#         finally:
-#             db.close()
-#
-#     my_app = create_app()
-#     my_app.dependency_overrides[get_db] = override_get_db
-#     return my_app
-#
-#
-# @pytest.fixture(scope="session")
-# def guest_client(app):
-#     return TestClient(app=app)
-#
-#
-# @pytest.fixture(scope="session")
-# def session():
-#     db = TestingSessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-#
-#
-# @pytest.fixture(scope="function")
-# def user(session):
-#     return create_user(
-#         db=session,
-#         form=UserCreate(
-#             username="tester",
-#             password="password",
-#         ),
-#     )
-#
-#
-# @pytest.fixture(scope="function")
-# def user2(session):
-#     return create_user(
-#         db=session,
-#         form=UserCreate(
-#             username="tester2",
-#             password="password",
-#         ),
-#     )
-#
-#
-# @pytest.fixture(scope="function")
-# def inactive_user(session):
-#     return create_user(
-#         db=session,
-#         form=UserCreate(
-#             username="inactiveuser",
-#             password="password",
-#         ),
-#     )
-#
-#
-# @pytest.fixture(scope="function")
-# def superuser(session, user):
-#     user.is_superuser = True
-#     session.commit()
-#     session.refresh(user)
-#     return user
-#
-#
-# @pytest.fixture(scope="function")
-# def inactive_superuser(session, superuser):
-#     superuser.is_active = False
-#     session.commit()
-#     session.refresh(superuser)
-#     return superuser
-#
-#
-# @pytest.fixture(scope="function")
-# def user_client(guest_client, user):
-#     access_token = create_access_token({"username": user.username})
-#     guest_client.headers = {"Authorization": "Bearer " + access_token}
-#     return guest_client
-#
-#
-# @pytest.fixture(scope="function")
-# def superuser_client(guest_client, superuser):
-#     access_token = create_access_token({"username": superuser.username})
-#     guest_client.headers = {"Authorization": "Bearer " + access_token}
-#     return guest_client
+import databases
+import pytest
+import pytest_asyncio
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from app.configs import ROOT_DIR
+from app.database import metadata
+from app.dependencies import get_db
+from app.domain.users.crud import create_user, patch_user
+from app.domain.users.schemas import UserCreate, UserPatch
+from app.main import create_app
+from app.security import create_access_token
+
+TEST_DATABASE_URL = f"sqlite+aiosqlite:///{ROOT_DIR / 'test.db'}"
+database = databases.Database(TEST_DATABASE_URL)
+
+
+@pytest_asyncio.fixture(autouse=True)
+async def clear_db_before_each_test():
+    engine = create_async_engine(TEST_DATABASE_URL, echo=True)
+    async with engine.begin() as conn:
+        await conn.run_sync(metadata.drop_all)
+        await conn.run_sync(metadata.create_all)
+        yield
+
+
+@pytest.fixture(scope="session")
+def app():
+    async def override_get_db():
+        async with database.transaction():
+            yield database
+
+    app = create_app()
+    app.dependency_overrides[get_db] = override_get_db
+    return app
+
+
+@pytest_asyncio.fixture
+async def guest_client(app):
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        yield ac
+
+
+@pytest_asyncio.fixture
+async def user():
+    return await create_user(database, UserCreate(username="tester", password="password"))
+
+
+@pytest_asyncio.fixture
+async def user2():
+    return await create_user(database, UserCreate(username="tester2", password="password"))
+
+
+@pytest_asyncio.fixture
+async def user_client(guest_client, user):
+    access_token = create_access_token({"username": user.username})
+    guest_client.headers = {"Authorization": "Bearer " + access_token}
+    return guest_client
+
+
+@pytest_asyncio.fixture
+async def superuser(user):
+    return await patch_user(database, user.username, UserPatch(is_superuser=True))
+
+
+@pytest_asyncio.fixture
+async def superuser_client(guest_client, superuser):
+    access_token = create_access_token({"username": superuser.username})
+    guest_client.headers = {"Authorization": "Bearer " + access_token}
+    return guest_client
+
+
+@pytest_asyncio.fixture
+async def inactive_user(user):
+    return await patch_user(database, user.username, UserPatch(is_active=False))
+
+
+@pytest_asyncio.fixture
+async def inactive_superuser(superuser):
+    return await patch_user(database, superuser.username, UserPatch(is_active=False))
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,265 +1,299 @@
-# from app.security import create_access_token
-#
-#
-# def test_create_user_without_username(user_client):
-#     response = user_client.post(
-#         url="/api/v1/users",
-#         json={
-#             "password": "password",
-#         },
-#     )
-#     assert response.status_code == 422
-#
-#
-# def test_create_user_without_password(user_client):
-#     response = user_client.post(
-#         url="/api/v1/users",
-#         json={
-#             "username": "tester",
-#         },
-#     )
-#     assert response.status_code == 422
-#
-#
-# def test_create_user_with_username_starting_with_non_alphabet(user_client):
-#     response = user_client.post(
-#         url="/api/v1/users",
-#         json={
-#             "username": "_tester",
-#             "password": "password",
-#         },
-#     )
-#     assert response.status_code == 422
-#
-#
-# def test_create_user_with_too_short_username(user_client):
-#     response = user_client.post(
-#         url="/api/v1/users",
-#         json={
-#             "username": "te",
-#             "password": "password",
-#         },
-#     )
-#     assert response.status_code == 422
-#
-#
-# def test_create_user(guest_client):
-#     response = guest_client.post(
-#         url="/api/v1/users",
-#         json={
-#             "username": "tester",
-#             "password": "password",
-#         },
-#     )
-#     assert response.status_code == 201
-#     assert response.json()["username"] == "tester"
-#     assert "password" not in response.json()
-#
-#
-# def test_get_users_by_user(user_client):
-#     response = user_client.get(url="/api/v1/users")
-#     assert response.status_code == 403
-#
-#
-# def test_get_users_by_superuser(superuser_client):
-#     response = superuser_client.get(url="/api/v1/users")
-#     assert response.status_code == 200
-#
-#
-# def test_get_user_404(superuser_client):
-#     response = superuser_client.get(url="/api/v1/users/nottester")
-#     assert response.status_code == 404
-#
-#
-# def test_get_user_403(user_client, user2):
-#     response = user_client.get(url=f"/api/v1/users/{user2.username}")
-#     assert response.status_code == 403
-#
-#
-# def test_get_user(user_client, user):
-#     response = user_client.get(url=f"/api/v1/users/{user.username}")
-#     assert response.status_code == 200
-#
-#
-# def test_get_other_user(superuser_client, user2):
-#     response = superuser_client.get(url=f"/api/v1/users/{user2.username}")
-#     assert response.status_code == 200
-#
-#
-# def test_update_user_without_password(user_client, user):
-#     response = user_client.put(
-#         url=f"/api/v1/users/{user.username}",
-#         json={
-#             "is_active": False,
-#             "is_superuser": False,
-#         },
-#     )
-#     assert response.status_code == 422
-#
-#
-# def test_update_user_404_by_user(user_client):
-#     response = user_client.put(
-#         url="/api/v1/users/nottester",
-#         json={
-#             "password": "password",
-#             "is_active": False,
-#             "is_superuser": False,
-#         },
-#     )
-#     assert response.status_code == 403
-#
-#
-# def test_update_user_404(superuser_client):
-#     response = superuser_client.put(
-#         url="/api/v1/users/nottester",
-#         json={
-#             "password": "password",
-#             "is_active": False,
-#             "is_superuser": False,
-#         },
-#     )
-#     assert response.status_code == 404
-#
-#
-# def test_update_user_403(user_client, user):
-#     response = user_client.put(
-#         url=f"/api/v1/users/{user.username}",
-#         json={
-#             "password": "password",
-#             "is_active": False,
-#             "is_superuser": False,
-#         },
-#     )
-#     assert response.status_code == 403
-#
-#
-# def test_update_user(superuser_client, user):
-#     response = superuser_client.put(
-#         url=f"/api/v1/users/{user.username}",
-#         json={
-#             "password": "password",
-#             "is_active": False,
-#             "is_superuser": False,
-#         },
-#     )
-#     assert response.status_code == 200
-#
-#
-# def test_patch_user_404_by_user(user_client):
-#     response = user_client.patch(
-#         url="/api/v1/users/nottester",
-#         json={
-#             "is_active": False,
-#         },
-#     )
-#     assert response.status_code == 403
-#
-#
-# def test_patch_user_itself(user_client, user):
-#     response = user_client.patch(
-#         url=f"/api/v1/users/{user.username}",
-#         json={
-#             "is_active": False,
-#         },
-#     )
-#     assert response.status_code == 200
-#
-#
-# def test_patch_user_with_is_superuser_by_user(user_client, user):
-#     response = user_client.patch(
-#         url=f"/api/v1/users/{user.username}",
-#         json={
-#             "is_superuser": True,
-#         },
-#     )
-#     assert response.status_code == 403
-#
-#
-# def test_patch_user_with_is_superuser_by_superuser(superuser_client, user):
-#     response = superuser_client.patch(
-#         url=f"/api/v1/users/{user.username}",
-#         json={
-#             "is_superuser": True,
-#         },
-#     )
-#     assert response.status_code == 200
-#
-#
-# def test_delete_user_404(user_client):
-#     response = user_client.delete(url="/api/v1/users/nottester")
-#     assert response.status_code == 403
-#
-#
-# def test_delete_user(user_client, user):
-#     response = user_client.delete(url=f"/api/v1/users/{user.username}")
-#     assert response.status_code == 204
-#
-#
-# def test_delete_other(superuser_client, user2):
-#     response = superuser_client.delete(url=f"/api/v1/users/{user2.username}")
-#     assert response.status_code == 204
-#
-#
-# def test_get_users_with_access_token_of_non_existent_user(guest_client, user):
-#     access_token = create_access_token({"username": "hello"})
-#     guest_client.headers = {"Authorization": "Bearer " + access_token}
-#     response = guest_client.get("/api/v1/users")
-#     assert response.status_code == 403
-#
-#
-# def test_get_users_with_access_token_of_inactive_user(guest_client, inactive_user):
-#     access_token = create_access_token({"username": inactive_user.username})
-#     guest_client.headers = {"Authorization": "Bearer " + access_token}
-#     response = guest_client.get("/api/v1/users")
-#     assert response.status_code == 403
-#
-#
-# def test_get_users_with_access_token_of_inactive_superuser(guest_client, inactive_superuser):
-#     access_token = create_access_token({"username": inactive_superuser.username})
-#     guest_client.headers = {"Authorization": "Bearer " + access_token}
-#     response = guest_client.get("/api/v1/users")
-#     assert response.status_code == 403
-#
-#
-# def test_get_user_with_access_token_of_inactive_superuser(guest_client, inactive_superuser, user):
-#     access_token = create_access_token({"username": inactive_superuser.username})
-#     guest_client.headers = {"Authorization": "Bearer " + access_token}
-#     response = guest_client.get(f"/api/v1/users/{user.username}")
-#     assert response.status_code == 403
-#
-#
-# def test_update_user_with_access_token_of_inactive_superuser(
-#     guest_client, inactive_superuser, user
-# ):
-#     access_token = create_access_token({"username": inactive_superuser.username})
-#     guest_client.headers = {"Authorization": "Bearer " + access_token}
-#     response = guest_client.put(
-#         url=f"/api/v1/users/{user.username}",
-#         json={
-#             "password": "password",
-#             "is_active": False,
-#             "is_superuser": False,
-#         },
-#     )
-#     assert response.status_code == 403
-#
-#
-# def test_patch_user_with_access_token_of_inactive_superuser(
-#     guest_client, inactive_superuser, user
-# ):
-#     access_token = create_access_token({"username": inactive_superuser.username})
-#     guest_client.headers = {"Authorization": "Bearer " + access_token}
-#     response = guest_client.patch(
-#         url=f"/api/v1/users/{user.username}",
-#         json={"is_active": False},
-#     )
-#     assert response.status_code == 403
-#
-#
-# def test_delete_user_with_access_token_of_inactive_superuser(
-#     guest_client, inactive_superuser, user
-# ):
-#     access_token = create_access_token({"username": inactive_superuser.username})
-#     guest_client.headers = {"Authorization": "Bearer " + access_token}
-#     response = guest_client.delete(url=f"/api/v1/users/{user.username}")
-#     assert response.status_code == 403
+import pytest
+
+from app.security import create_access_token
+
+
+@pytest.mark.asyncio
+async def test_create_user_without_username(user_client):
+    response = await user_client.post(
+        url="/api/v1/users",
+        json={
+            "password": "password",
+        },
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_user_without_password(user_client):
+    response = await user_client.post(
+        url="/api/v1/users",
+        json={
+            "username": "tester",
+        },
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_user_with_username_starting_with_non_alphabet(user_client):
+    response = await user_client.post(
+        url="/api/v1/users",
+        json={
+            "username": "_tester",
+            "password": "password",
+        },
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_user_with_too_short_username(user_client):
+    response = await user_client.post(
+        url="/api/v1/users",
+        json={
+            "username": "te",
+            "password": "password",
+        },
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_user(guest_client):
+    response = await guest_client.post(
+        url="/api/v1/users",
+        json={
+            "username": "tester",
+            "password": "password",
+        },
+    )
+    assert response.status_code == 201
+    assert response.json()["username"] == "tester"
+    assert "password" not in response.json()
+
+
+@pytest.mark.asyncio
+async def test_get_users_by_user(user_client):
+    response = await user_client.get(url="/api/v1/users")
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_get_users_by_superuser(superuser_client):
+    response = await superuser_client.get(url="/api/v1/users")
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_get_user_404(superuser_client):
+    response = await superuser_client.get(url="/api/v1/users/nottester")
+    assert response.status_code == 404
+
+
+@pytest.mark.asyncio
+async def test_get_user_403(user_client, user2):
+    response = await user_client.get(url=f"/api/v1/users/{user2.username}")
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_get_user(user_client, user):
+    response = await user_client.get(url=f"/api/v1/users/{user.username}")
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_get_other_user(superuser_client, user2):
+    response = await superuser_client.get(url=f"/api/v1/users/{user2.username}")
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_update_user_without_password(user_client, user):
+    response = await user_client.put(
+        url=f"/api/v1/users/{user.username}",
+        json={
+            "is_active": False,
+            "is_superuser": False,
+        },
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_update_user_404_by_user(user_client):
+    response = await user_client.put(
+        url="/api/v1/users/nottester",
+        json={
+            "password": "password",
+            "is_active": False,
+            "is_superuser": False,
+        },
+    )
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_update_user_404(superuser_client):
+    response = await superuser_client.put(
+        url="/api/v1/users/nottester",
+        json={
+            "password": "password",
+            "is_active": False,
+            "is_superuser": False,
+        },
+    )
+    assert response.status_code == 404
+
+
+@pytest.mark.asyncio
+async def test_update_user_403(user_client, user):
+    response = await user_client.put(
+        url=f"/api/v1/users/{user.username}",
+        json={
+            "password": "password",
+            "is_active": False,
+            "is_superuser": False,
+        },
+    )
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_update_user(superuser_client, user):
+    response = await superuser_client.put(
+        url=f"/api/v1/users/{user.username}",
+        json={
+            "password": "password",
+            "is_active": False,
+            "is_superuser": False,
+        },
+    )
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_patch_user_404_by_user(user_client):
+    response = await user_client.patch(
+        url="/api/v1/users/nottester",
+        json={
+            "is_active": False,
+        },
+    )
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_patch_user_itself(user_client, user):
+    response = await user_client.patch(
+        url=f"/api/v1/users/{user.username}",
+        json={
+            "is_active": False,
+        },
+    )
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_patch_user_with_is_superuser_by_user(user_client, user):
+    response = await user_client.patch(
+        url=f"/api/v1/users/{user.username}",
+        json={
+            "is_superuser": True,
+        },
+    )
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_patch_user_with_is_superuser_by_superuser(superuser_client, user):
+    response = await superuser_client.patch(
+        url=f"/api/v1/users/{user.username}",
+        json={
+            "is_superuser": True,
+        },
+    )
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_delete_user_404(user_client):
+    response = await user_client.delete(url="/api/v1/users/nottester")
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_delete_user(user_client, user):
+    response = await user_client.delete(url=f"/api/v1/users/{user.username}")
+    assert response.status_code == 204
+
+
+@pytest.mark.asyncio
+async def test_delete_other(superuser_client, user2):
+    response = await superuser_client.delete(url=f"/api/v1/users/{user2.username}")
+    assert response.status_code == 204
+
+
+@pytest.mark.asyncio
+async def test_get_users_with_access_token_of_non_existent_user(guest_client, user):
+    access_token = create_access_token({"username": "hello"})
+    guest_client.headers = {"Authorization": "Bearer " + access_token}
+    response = await guest_client.get("/api/v1/users")
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_get_users_with_access_token_of_inactive_user(guest_client, inactive_user):
+    access_token = create_access_token({"username": inactive_user.username})
+    guest_client.headers = {"Authorization": "Bearer " + access_token}
+    response = await guest_client.get("/api/v1/users")
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_get_users_with_access_token_of_inactive_superuser(guest_client, inactive_superuser):
+    access_token = create_access_token({"username": inactive_superuser.username})
+    guest_client.headers = {"Authorization": "Bearer " + access_token}
+    response = await guest_client.get("/api/v1/users")
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_get_user_with_access_token_of_inactive_superuser(
+    guest_client, inactive_superuser, user
+):
+    access_token = create_access_token({"username": inactive_superuser.username})
+    guest_client.headers = {"Authorization": "Bearer " + access_token}
+    response = await guest_client.get(f"/api/v1/users/{user.username}")
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_update_user_with_access_token_of_inactive_superuser(
+    guest_client, inactive_superuser, user
+):
+    access_token = create_access_token({"username": inactive_superuser.username})
+    guest_client.headers = {"Authorization": "Bearer " + access_token}
+    response = await guest_client.put(
+        url=f"/api/v1/users/{user.username}",
+        json={
+            "password": "password",
+            "is_active": False,
+            "is_superuser": False,
+        },
+    )
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_patch_user_with_access_token_of_inactive_superuser(
+    guest_client, inactive_superuser, user
+):
+    access_token = create_access_token({"username": inactive_superuser.username})
+    guest_client.headers = {"Authorization": "Bearer " + access_token}
+    response = await guest_client.patch(
+        url=f"/api/v1/users/{user.username}",
+        json={"is_active": False},
+    )
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_delete_user_with_access_token_of_inactive_superuser(
+    guest_client, inactive_superuser, user
+):
+    access_token = create_access_token({"username": inactive_superuser.username})
+    guest_client.headers = {"Authorization": "Bearer " + access_token}
+    response = await guest_client.delete(url=f"/api/v1/users/{user.username}")
+    assert response.status_code == 403
